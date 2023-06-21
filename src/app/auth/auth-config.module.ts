6a1a26ff031655c4a9fb45ea01d61c13c2315414
Karim <karimtaha881@gmail.com>
2023-06-21 21:05:03 +0300

    profile screen

--- src/app/auth/auth-config.module.ts
+++ src/app/auth/auth-config.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
-import { AuthModule } from 'angular-auth-oidc-client';
+import {NgModule} from '@angular/core';
+import {AuthModule} from "@angular/fire/auth";
 
-
-@NgModule({
-    imports: [AuthModule.forRoot({
-        config: {
-            authority: 'localhost',
-            redirectUrl: window.location.origin,
-            postLogoutRedirectUri: window.location.origin,
-            clientId: 'please-enter-clientId',
-            scope: 'please-enter-scopes', // 'openid profile ' + your scopes
-            responseType: 'code',
-            silentRenew: true,
-            silentRenewUrl: window.location.origin + '/silent-renew.html',
-            renewTimeBeforeTokenExpiresInSeconds: 10,
-        }
-      })],
-    exports: [AuthModule],
-})
-export class AuthConfigModule {}
+// @NgModule({
+//   imports: [AuthModule.forRoot({
+//     config: {
+//       authority: 'localhost',
+//       redirectUrl: window.location.origin,
+//       postLogoutRedirectUri: window.location.origin,
+//       clientId: 'please-enter-clientId',
+//       scope: 'please-enter-scopes', // 'openid profile ' + your scopes
+//       responseType: 'code',
+//       silentRenew: true,
+//       silentRenewUrl: window.location.origin + '/silent-renew.html',
+//       renewTimeBeforeTokenExpiresInSeconds: 10,
+//     }
+//   })],
+//   exports: [AuthModule],
+// })
+export class AuthConfigModule {
+}
