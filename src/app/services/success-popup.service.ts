52855bd5a928481429e865af4ee3652722b7e104
adelahmedd <32197710+aadelaahmed@users.noreply.github.com>
2023-06-22 19:39:44 +0300

    -add pop up message for user when add new appartment.

---
+++ src/app/services/success-popup.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import Swal from "sweetalert2";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SuccessPopupService {
+
+  constructor() { }
+
+  showSuccessPopupMenu(){
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Your property will be reviewed before publishing.',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+}
