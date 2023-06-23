a8f0b7bb550d2fcaa1dcaa679e82ddc7b80c4c30
Karim <karimtaha881@gmail.com>
2023-06-23 05:15:46 +0300

    sign in with google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,14 +13,18 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {LoginComponent} from './components/user/login/login.component';
 import {RegisterComponent} from './components/user/register/register.component';
 import {HttpClientModule} from "@angular/common/http";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ForgetPasswordComponent } from './components/user/forget-password/forget-password.component';
-import { ResetPasswordComponent } from './components/user/reset-password/reset-password.component';
-import { ProfileComponent } from './components/user/profile/profile.component';
-import { ConfirmationCodeComponent } from './components/user/confirmation-code/confirmation-code.component';
-import {SharedService} from "./services/shared.service";
-import { EditProfileComponent } from './components/user/edit-profile/edit-profile.component';
-import { UpdatePasswordComponent } from './components/user/update-password/update-password.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ForgetPasswordComponent} from './components/user/forget-password/forget-password.component';
+import {ResetPasswordComponent} from './components/user/reset-password/reset-password.component';
+import {ProfileComponent} from './components/user/profile/profile.component';
+import {ConfirmationCodeComponent} from './components/user/confirmation-code/confirmation-code.component';
+import {EditProfileComponent} from './components/user/edit-profile/edit-profile.component';
+import {UpdatePasswordComponent} from './components/user/update-password/update-password.component';
+import {SocialLoginModule, SocialAuthServiceConfig, GoogleSigninButtonModule} from '@abacritt/angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from '@abacritt/angularx-social-login';
 
 @NgModule({
   declarations: [
@@ -47,8 +51,32 @@ import { UpdatePasswordComponent } from './components/user/update-password/updat
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
+    SocialLoginModule,
+    GoogleSigninButtonModule
   ],
-  providers: [SharedService],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '248555964328-9ch256vbo4bc3fdcheqf6iiqt7pe88na.apps.googleusercontent.com'
+            )
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('251985627474760')
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,12 +1,18 @@
-import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {LoginDTO} from "../../../dtos/users/login-dto.model";
 import {ApiService} from "../../../services/api.service";
 import {Router} from "@angular/router";
 import {PopupService} from "../../../services/popup.service";
 import {HttpHeaders} from "@angular/common/http";
-import {SharedService} from "../../../services/shared.service";
 import {NavbarService} from "../../../services/navbar.service";
+import {
+  FacebookLoginProvider,
+  GoogleLoginProvider,
+  SocialAuthService,
+  SocialUser
+} from "@abacritt/angularx-social-login";
+import {TokenDto} from "../../../dtos/users/token-dto.model";
 
 @Component({
   selector: 'app-login',
@@ -15,10 +21,9 @@ import {NavbarService} from "../../../services/navbar.service";
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-
-  title = 'loginGoogle';
-  auth2: any;
-  @ViewChild('loginRef', {static: true}) loginElement!: ElementRef;
+  user: SocialUser;
+  loggedIn: boolean;
+  private accessToken = '';
 
   constructor(
     private formBuilder: FormBuilder,
@@ -26,6 +31,7 @@ export class LoginComponent implements OnInit {
     private popupService: PopupService,
     private navbarService: NavbarService,
     private router: Router,
+    private authService: SocialAuthService
   ) {
   }
 
@@ -34,8 +40,35 @@ export class LoginComponent implements OnInit {
       email: [null, [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
       password: [null, Validators.required]
     });
-    // this.googleAuthSDK();
+
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+      if (this.loggedIn) {
+        this.handleGoogleLogin(user.idToken);
+      }
+      console.log("user -->> ", user);
+    });
   }
+
+  handleGoogleLogin(idToken: string): void {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    const token: TokenDto = new TokenDto(idToken);
+    this.apiService.post("api/google", token, {headers, withCredentials: true}).subscribe({
+      next: response => {
+        this.popupService.successPopup("Welcome, " + response.payload.firstName);
+        this.router.navigate(['/home']);
+      },
+      error: err => {
+        this.popupService.errorPopup("Must first register");
+        this.router.navigate(['/register'])
+      }
+    });
+  }
+
 
   signInButton() {
     if (this.loginForm.valid) {
@@ -53,70 +86,36 @@ export class LoginComponent implements OnInit {
             this.router.navigate(['/home']);
           },
           error: err => {
-            this.popupService.errorPopup(err.payload);
+            this.popupService.errorPopup("Invalid email or password");
           }
         }
       )
     }
   }
 
+  signOut(): void {
+    this.authService.signOut();
+  }
 
-  // signInUsingGoogleButton() {
-  //   const provider = new GoogleAuthProvider();
-  //   provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-  //   const auth = getAuth();
-  //   signInWithPopup(auth, provider)
-  //     .then((result) => {
-  //       const credential = GoogleAuthProvider.credentialFromResult(result);
-  //       const token = credential.accessToken;
-  //       const user = result.user;
-  //
-  //     }).catch((error) => {
-  //     const errorCode = error.code;
-  //     const errorMessage = error.message;
-  //     const email = error.customData.email;
-  //     const credential = GoogleAuthProvider.credentialFromError(error);
-  //     // ...
-  //   });
-  // }
+  refreshToken(): void {
+    this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
+  }
 
+  getAccessToken(): void {
+    this.authService.getAccessToken(GoogleLoginProvider.PROVIDER_ID).then(accessToken => this.accessToken = accessToken);
+  }
 
-  // callGoogleLoginButton() {
-  //   this.auth2.attachClickHandler(this.loginElement.nativeElement, {}, (googleAuthUser: any) => {
-  //     let profile = googleAuthUser.getBasicProfile();
-  //     console.log('Token || ' + googleAuthUser.getAuthResponse().id_token);
-  //     console.log('ID: ' + profile.getId());
-  //     console.log('Name: ' + profile.getName());
-  //     console.log('Image URL: ' + profile.getImageUrl());
-  //     console.log('Email: ' + profile.getEmail());
-  //   }, (error: any) => {
-  //     alert(JSON.stringify(error, undefined, 2));
-  //   });
-  // }
-  //
-  // googleAuthSDK() {
-  //
-  //   (<any>window)['googleSDKLoaded'] = () => {
-  //     (<any>window)['gapi'].load('auth2', () => {
-  //       this.auth2 = (<any>window)['gapi'].auth2.init({
-  //         client_id: '248555964328-9ch256vbo4bc3fdcheqf6iiqt7pe88na.apps.googleusercontent.com',
-  //         cookiepolicy: 'single_host_origin',
-  //         scope: 'profile email'
-  //       });
-  //       this.callGoogleLoginButton();
-  //     });
-  //   }
-  //
-  //   (function (d, s, id) {
-  //     var js, fjs = d.getElementsByTagName(s)[0];
-  //     if (d.getElementById(id)) {
-  //       return;
-  //     }
-  //     js = d.createElement('script');
-  //     js.id = id;
-  //     js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
-  //     fjs?.parentNode?.insertBefore(js, fjs);
-  //   }(document, 'script', 'google-jssdk'));
-  // }
+  getGoogleCalendarData(): void {
+    if (!this.accessToken) return;
+
+    this.apiService
+      .get('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
+        headers: {Authorization: `Bearer ${this.accessToken}`},
+      })
+      .subscribe((events) => {
+        alert('Look at your console');
+        console.log('events', events);
+      });
+  }
 
 }
