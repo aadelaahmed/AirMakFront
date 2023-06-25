fdb7dac7cc99abfec34205c54e5422cd7c53a0ba
Mahmoud Geneidi <mahmoudgeneidi98@gmail.com>
2023-06-25 06:30:49 +0300

    some changes

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -43,7 +43,7 @@ export class authGuard implements CanActivate {
         const allowedNotAdminOrUserRoutes = [
           '/user/home',
           '/login',
-          '/register',
+          '/user/register',
           '/user/property/discovery',
           '/user/packages'
         ];
