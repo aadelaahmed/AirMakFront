efe9e5d479ce666ddc35b65c03a21a0cbecbbe02
Karim <karimtaha881@gmail.com>
2023-06-25 16:23:04 +0300

    edit footer

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {ApiService} from "../../../services/api.service";
+import {SocialAuthService} from "@abacritt/angularx-social-login";
+import {PopupService} from "../../../services/popup.service";
+import {SessionStorageService} from "../../../services/session-storage.service";
+import {AuthGuardService} from "../../../services/authGuard.service";
+import {SubscriptionService} from "../../../services/subscription.service";
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +13,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  constructor(
+    private authGuardService: AuthGuardService,
+  ) {
+  }
+  get isSignInButtonVisible(): boolean {
+    return !this.authGuardService.isLoggedIn("userID");
+  }
 
+  get getRole(): any {
+    return this.authGuardService.getRole();
+  }
 }
