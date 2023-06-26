df738eeb8dfa22343bd52cb5926586b1f60edd6b
Mohamed Gomaa <62193989+mohamedgomaa23722@users.noreply.github.com>
2023-06-26 08:30:43 +0200

    hggh

--- src/app/interface/user.ts
+++ src/app/interface/user.ts
@@ -4,15 +4,15 @@ import { Subscription } from './subscription';
 
 export class User {
   id: number;
-   firstName: string;
-   lastName: string;
-   birthdate: Date;
+  firstName: string;
+  lastName: string;
+  birthdate: Date;
   phoneNumber: string;
-   email: string;
-   password: string;
-   properties: Property[];
-   reviews: Review[];
-   subscriptions: Subscription[];
-
+  email: string;
+  password: string;
+  properties: Property[];
+  reviews: Review[];
+  subscriptions: Subscription[];
+  propertycount:number;
   // Constructors, getters, and setters
 }
